c607c3caf238d38962989784ffa90e046087c091
jsampera <jsamperarias@hotmail.com>
2021-05-30 20:48:01 +0200

    testing publish

---
+++ .frontech/styledictionary.js
@@ -0,0 +1,244 @@
+const [utils, symbols, webfont, css] = [
+  require("./utils"),
+  require("log-symbols"),
+  require("webfont").default,
+  require("./postcss")
+];
+
+
+
+module.exports.styleDictionary = (data, file) => {
+  let grid = [];
+  const checkStencilUtils = (property,type) => property.sassConfig ? {[type]: property.sassConfig[type]} : {[type]:property[type]};
+  const StyleDictionary = require("style-dictionary").extend(
+		{
+			source: [file],
+			...utils.buildPlatforms(data.configuration)
+		}
+  );
+
+  const generateIconFont = async (svg) => {
+    const { name, input, output } = svg;
+    webfont({
+      files: `${process.cwd()}/${input}/*.svg`,
+      fontName: name,
+      template: "scss",
+      dest: `${process.cwd()}/library/web/settings/_icons.scss`,
+      templateClassName: "icon",
+      templateFontPath: "#{$font-path}",
+      fontWeight: 800
+    })
+      .then((result) => {
+        const file = (folder, file, data) => {
+          utils.createFile(folder, file, data);
+          console.log(`${symbols.success}  ${folder}/${file}`);
+        };
+        const pathFile = data.configuration.customPath
+          ? `${process.cwd()}/${
+              data.configuration.customPath
+            }/library/web/utilities`
+          : `${process.cwd()}/library/web/utilities`;
+        console.log(
+          `\nIconic font creation based on the svg files in the path ${input}`
+        );
+        file(
+          pathFile,
+          `_icons.scss`,
+          `@use '../settings/general' as *;\n${result.template}`
+        );
+        file(
+          `${process.cwd()}/${output}`,
+          `${result.config.fontName}.svg`,
+          result.svg
+        );
+        file(
+          `${process.cwd()}/${output}`,
+          `${result.config.fontName}.ttf`,
+          result.ttf
+        );
+        file(
+          `${process.cwd()}/${output}`,
+          `${result.config.fontName}.eot`,
+          result.eot
+        );
+        file(
+          `${process.cwd()}/${output}`,
+          `${result.config.fontName}.woff`,
+          result.woff
+        );
+        data.configuration.outputCSS ? css.buildCSS(data) : null;
+        utils.messages.print("Settings creation process finished");
+      })
+      .catch((e) => {
+        utils.messages.error(
+          `\nCheck the configuration file, you have established the following information:\n\n${JSON.stringify(
+            svg,
+            null,
+            2
+          )}`
+        );
+        utils.createFile(
+          `${process.cwd()}/library/web/utilities`,
+          `_icons.scss`,
+          `// To generate the iconic font, check the configuration file ${file}`
+        );
+      });
+  };
+  StyleDictionary.registerFormat({
+    name: "custom/grid",
+    formatter: (dictionary) => {
+      const property = checkStencilUtils(dictionary.properties,'grid');
+      try {
+        let result = [];
+        for (const key in property.grid) {
+          let value = property.grid[key];
+          grid.push(key);
+          layout = value.gutter.attributes.type;
+          const [gutter, offset, columns, width] = [
+            value.gutter,
+            value.offset,
+            value.columns,
+            value.width
+          ];
+          result += `${layout}:(
+              ${gutter.path[2]}:${gutter.value},
+              ${offset.path[2]}:${offset.value},
+              ${columns.path[2]}:${columns.value},
+              ${width.path[2]}:${width.value}
+            ),`;
+
+        }
+
+        return `/// Dynamically created map based on the configuration file. Define the breakpoints of the different breakpoints\n/// @type map\n/// @group grid \n$breakpoints: (
+                  ${result}
+              );`;
+      } catch {
+        utils.messages.error(
+          `${symbols.error}  No grid utility configuration specified. The file will be created without content. Please check the configuration file ${file}.`
+        );
+        return `// To generate the grid configuration, check the configuration file ${file}\n$breakpoints:()!default;`;
+      }
+    }
+  });
+  StyleDictionary.registerFormat({
+    name: "custom/mediaqueries",
+    formatter: (dictionary) => {
+      const property = checkStencilUtils(dictionary.properties,'grid');
+      try {
+        let result = [];
+        for (const key in property.grid) {
+          let value = property.grid[key];
+          layout = value.gutter.attributes.type;
+
+          const [width] = [value.width.value];
+          result += `/// Mixin whose objective is to create the media-query based on the cut points established in the configuration file\n///\n///\n/// @example scss\n///\n///      .test{\n///         width: 100%;\n///         @include screen-${key}(){\n///           width: auto;\n///         }\n///      }\n///\n/// @example css\n///\n///      .test {\n///         width: 100%;\n///       }\n///\n///      @media only screen and (min-width: ${width}) {\n///         .test {\n///           width: auto;\n///         }\n///      }\n///\n/// @group media-queries \n@mixin screen-${key}{\n   @media only screen and (min-width: ${width}) {\n     @content\n   }\n};\n`;
+        }
+
+
+        return (result += `/// Mixin whose objective is to create the media-query based on the cut points established in the configuration file\n///\n///\n/// @example scss\n///\n///      .test{\n///         width: 100%;\n///         @include screen-custom($screen){\n///           width: auto;\n///         }\n///      }\n///\n/// @example css\n///\n///      .test {\n///         width: 100%;\n///       }\n///\n///      @media only screen and (min-width: $screen) {\n///         .test {\n///           width: auto;\n///         }\n///      }\n///\n/// @group media-queries \n@mixin screen-custom($screen){\n   @media only screen and (min-width: $screen) {\n     @content\n   }\n};\n`);
+
+      } catch (err) {
+        utils.messages.error(
+          `${symbols.error}  No grid utility configuration specified. The file will be created without content. Please check the configuration file ${file}.`
+        );
+        return `// To generate the mixin of mediaqueries, check the configuration file ${file}`;
+      }
+    }
+  });
+  StyleDictionary.registerFormat({
+    name: "custom/spacing",
+    formatter: (dictionary) => {
+      try {
+        const property = checkStencilUtils(dictionary.properties,'spacing');
+        const { increase, limit } = property.spacing;
+
+        return `/// Dynamically created map based on the configuration file. Define the attributes to create the margin and padding utility classes\n/// @type number\n/// @group spacing
+          $spacing: (
+              increase:${increase.value},
+              limit:${limit.value}
+          );
+        `;
+      } catch {
+        utils.messages.error(
+          `${symbols.error}  No margin and padding utility settings are specified. The file will be created without content. Please check the configuration file ${file}.`
+        );
+        return `// To generate the margin and padding utilities, check the configuration file ${file}\n$spacing:() !default;`;
+      }
+    }
+  });
+  StyleDictionary.registerFormat({
+    name: "custom/properties-color",
+    formatter: (dictionary) => {
+      try {
+        const property = checkStencilUtils(dictionary.properties,'color');
+        let key = Object.keys(property.color);
+        let customProperties = "\n";
+        key.forEach((item) => {
+          value = property.color[item];
+          customProperties += `--${item}:${value.value};\n`;
+        });
+        return `/// Color variables defined in the ${file} file\n///@group colors\n:root{${customProperties}};`;
+      } catch(e) {
+        utils.messages.error(
+          `${symbols.error}  No color settings have been specified. The file will be created without content. Please check the configuration file ${file}.`
+        );
+        return `// To generate the custom properties of colors, check the configuration file ${file}`;
+      }
+    }
+  });
+
+  StyleDictionary.registerFormat({
+    name: "custom/properties-typography",
+    formatter: (dictionary) => {
+      try {
+        const property = checkStencilUtils(dictionary.properties,'typography');
+        let key = Object.keys(property.typography);
+        let fonts = "";
+        let customProperties = "";
+
+        key.forEach((font) => {
+          value = property.typography[font];
+          fonts += `\n${font}: (\nname:${value.family.value},\nweight:${value.weight.value},\nstyle:${value.style.value}\n),`;
+          customProperties += `--${font}:${value.family.value};\n`;
+        });
+        return `/// Font map defined in the ${file} file\n///@group fonts\n$fonts:(${fonts});\n\n/// Custom properties cuyo valor es el nombre aportado en el fichero ${file}\n/// @group fonts\n:root{\n${customProperties}};`;
+      } catch (error) {
+        utils.messages.error(
+          `${symbols.error}  No font settings have been specified. The file will be created without content. Please check the configuration file ${file}.`
+        );
+        return `// To generate the text sources, check the configuration file ${file}\n$fonts:() !default;`;
+      }
+    }
+  });
+  StyleDictionary.registerFormat({
+    name: "custom/properties-icons",
+    formatter: (dictionary) => {
+      try {
+        const property = checkStencilUtils(dictionary.properties,'typography');
+        let key = Object.keys(property.typography);
+        let icon;
+
+        key.forEach((font) => {
+          value = property.typography[font];
+          value.family.input && value.family.output
+            ? (icon = {
+                name: font,
+                input: value.family.input,
+                output: value.family.output
+              })
+            : utils.messages.warning(
+                `${symbols.warning}  Review the configuration file. For the creation of the iconic source you have entered the source path ${value.family.input} and the exit route ${value.family.output}`
+              );
+        });
+        generateIconFont(icon);
+      } catch (error) {
+        utils.messages.error(
+          `${symbols.error}  No iconic font settings have been specified. The file will be created without content. Please check the configuration file ${file}.`
+        );
+        return `// To generate the iconic font, check the configuration file ${file}`;
+      }
+    }
+  });
+
+  StyleDictionary.buildAllPlatforms();
+};
---
+++ README.md
@@ -0,0 +1,218 @@
+# Design Systems Utils
+
+Design Systems Utils aims to provide a series of common utilities at the beginning of any web project, which will facilitate and speed up the day-to-day development. For more information click on the following [link](https://front-tech.github.io/design-system-utils/).
+
+To use the library we must follow the following steps:
+1. Insert the following line of code in the .npmrc file ```@front-tech:registry=https://npm.pkg.github.com```
+2. Run the command ```npm install @front-tech/design-systems-utils -D```
+3. Create a script in the package.json where we will pass the configuration file ```design-systems-utils --file=frontech.json```
+
+For the proper functioning of the library, it is necessary to create a configuration file in the project that will determine the margin, padding and custom grid utilities. This configuration must maintain the following structure:
+
+```
+{
+  "color": {
+    "secondary_3": {
+      "value": "rgba(203.00000309944153, 223.00000190734863, 144.00000661611557, 1)",
+      "type": "color"
+    },
+    "secondary_2": {
+      "value": "rgba(143.00000667572021, 173.00000488758087, 136.00000709295273, 1)",
+      "type": "color"
+    },
+    "secondary_1": {
+      "value": "rgba(127.00000002980232, 156.00000590085983, 150.0000062584877, 1)",
+      "type": "color"
+    },
+    "primary_3": {
+      "value": "rgba(155.00000596046448, 193.00000369548798, 219.0000021457672, 1)",
+      "type": "color"
+    },
+    "primary_2": {
+      "value": "rgba(20.000000707805157, 131.00000739097595, 187.00000405311584, 1)",
+      "type": "color"
+    },
+    "primary_1": {
+      "value": "rgba(0, 102.00000151991844, 153.00000607967377, 1)",
+      "type": "color"
+    }
+  },
+  "typography": {
+    "Helvetica--Neue--35": {
+      "family": {
+        "value": "helvetica--Neue--35",
+        "type": "typography"
+      },
+      "weight": {
+        "value": 400,
+        "type": "typography"
+      },
+      "style":{
+        "value":"normal",
+        "type":"typography"
+      }
+    },
+    "Helvetica--Neue--55": {
+      "family": {
+        "value": "helvetica--Neue--55",
+        "type": "typography"
+      },
+      "weight": {
+        "value": 400,
+        "type": "typography"
+      },
+      "style":{
+        "value":"normal",
+        "type":"typography"
+      }
+    },
+    "Lato-regular": {
+      "family": {
+        "value": "lato-regular",
+        "type": "typography"
+      },
+      "weight": {
+        "value": 400,
+        "type": "typography"
+      },
+      "style":{
+        "value":"normal",
+        "type":"typography"
+      }
+    },
+    "Gotham-italic": {
+      "family": {
+        "value": "gotham-italic",
+        "type": "typography"
+      },
+      "weight": {
+        "value": 300,
+        "type": "typography"
+      },
+      "style":{
+        "value":"italic",
+        "type":"typography"
+      }
+    },
+    "icomoon": {
+      "family": {
+        "value": "icomoon",
+        "input": "assets/icons",
+        "output": "assets/fonts",
+        "type": "typography"
+      },
+      "weight": {
+        "value": "normal",
+        "type": "typography"
+      },
+      "style":{
+        "value":"normal",
+        "type":"typography"
+      }
+    }
+  },
+  "grid": {
+    "sm": {
+      "gutter": {
+        "value": "30px",
+        "type": "grid"
+      },
+      "offset": {
+        "value": "50px",
+        "type": "grid"
+      },
+      "columns": {
+        "value": 4,
+        "type": "grid"
+      },
+      "width": {
+        "value": "360px",
+        "type": "grid"
+      }
+    },
+    "md": {
+      "gutter": {
+        "value": "30px",
+        "type": "grid"
+      },
+      "offset": {
+        "value": "50px",
+        "type": "grid"
+      },
+      "columns": {
+        "value": 6,
+        "type": "grid"
+      },
+      "width": {
+        "value": "768px",
+        "type": "grid"
+      }
+    },
+    "lg": {
+      "gutter": {
+        "value": "30px",
+        "type": "grid"
+      },
+      "offset": {
+        "value": "50px",
+        "type": "grid"
+      },
+      "columns": {
+        "value": 12,
+        "type": "grid"
+      },
+      "width": {
+        "value": "1440px",
+        "type": "grid"
+      }
+    }
+  },
+  "spacing":{
+    "increase":{
+        "value": 5,
+        "type":"spacing"
+    },
+    "limit": {
+        "value": 20,
+        "type":"spacing"
+    }
+  },
+  "configuration": {
+    "outputCSS": {
+      "path": "style.css"
+    },
+    "platforms": ["scss", "ios", "android"],
+    "customPath": "assets/styles"
+  }
+}
+```
+
+
+We can also generate a css output of the library specifying in the configuration file the path where we want to export said file:
+
+```
+"configuration": {
+    "outputCSS": {
+      "path": "style.css"
+    },
+    "platforms": ["scss", "ios", "android"],
+    "customPath": "assets/styles"
+}
+```
+
+## Contributing to Front-tech
+
+Thanks for contributing to front-tech development!
+Feature requests and bug reports can be filed on [Github](https://github.com/front-tech/design-system-utils)
+
+If you are contributing code with new features or bug-fixes:
+- Fork the project, and create a branch for your contribution.
+- Follow the development guide below to get design-systems-utils to work.
+- Write the necessary documentation in ```library/web/**/*.scss```.
+- Open a pull request on [Github](https://github.com/front-tech/design-system-utils/issues)
+
+## Development
+To install the necessary Node dependencies, run ```npm install```.
+
+You can start up a local development server with ```npm run serve```.
+Access the running server at ```http://localhost:3000```.
